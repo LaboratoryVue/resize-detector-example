f9608e9a2d12040e003f4d06a9e13b1086b62852
gearmobile <gearmobile@gmail.com>
2018-08-16 11:18:53 +0300

    fix

---
+++ src/components/StartPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="start">
+    <section ref="block" class="block"></section>
+  </div>
+</template>
+
+<script>
+import { addListener, removeListener } from 'resize-detector'
+
+export default {
+  name: 'StartPage',
+  methods: {
+    onEventRise() {
+      addListener(this.$refs.block, () => console.log(`resize start`))
+    },
+    onEventDown() {
+      removeListener(this.$refs.block, () => console.log(`resize down`))
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.block {
+  width: 100%;
+  min-width: 100%;
+}
+</style>
